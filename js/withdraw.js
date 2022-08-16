92a07cc79988c7ef2f9d0037e8415348df2c11e4
Sayeednayeem1 <sayeedn850@gmail.com>
2022-08-16 13:09:32 +0600

    final push

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,6 +4,16 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // step-7:
+    withdrawField.value = "";
+
+
+    if(isNaN(newWithdrawAmount)){
+        alert("please provide a valid number");
+        return;
+    }
+
     // step-3:
 
     const withdrawTotalElement = document.getElementById('withdraw-total');
@@ -11,20 +21,25 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
     
     
-    // step-4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     // step-5:
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    
+
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert("Baap er bank a etho taka nai");
+        return;
+    }
+
+    // step-4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     // step-6:
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
     
-    // step-7:
-    withdrawField.value = "";
-
+    
 })
